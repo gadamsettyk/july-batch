64949ccb1cfa74fd435b08b771a4360f4b0b4f53
gadamsettyk <113530349+gadamsettyk@users.noreply.github.com>
2022-09-14 19:54:27 +0530

    Update A.java

--- A.java
+++ A.java
@@ -16,36 +16,3 @@ class A
 		System.out.println(i);
 	}
 }
-
-
-/*
-1
-1
-2
-2
-2
-2
-2
-2
-2
-2
-2
-
-
---------------------
-an opertator which is brining  one unit effect in the variable
-there are two types of unary operator are available
-
-++   ====>    increment unary operator
---   ====>    decrement unary operator
-both unary operataors can be used in the two differnert ways
-
-1. post way of useing
-
-2. pre way of useing
-post awy of useing the unariy operator
-modifed value or the changed value  operated value should effect after the operatino not at the place of the operation
-
-comment content will not be presrnt inthe java program
-
-*/
