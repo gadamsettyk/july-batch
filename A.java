fd50769d6e07f1a785c2166fbe8ab7f008dde045
gadamsettyk <113530349+gadamsettyk@users.noreply.github.com>
2022-09-14 19:53:27 +0530

    drag1 commited
    commited  commited

---
+++ A.java
@@ -0,0 +1,51 @@
+class A 
+{
+	public static void main(String[] args) 
+	{
+		int i = 1;
+		System.out.println(i);
+		System.out.println(i++);  //post awy of useing the unariy operator 
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+	}
+}
+
+
+/*
+1
+1
+2
+2
+2
+2
+2
+2
+2
+2
+2
+
+
+--------------------
+an opertator which is brining  one unit effect in the variable
+there are two types of unary operator are available
+
+++   ====>    increment unary operator
+--   ====>    decrement unary operator
+both unary operataors can be used in the two differnert ways
+
+1. post way of useing
+
+2. pre way of useing
+post awy of useing the unariy operator
+modifed value or the changed value  operated value should effect after the operatino not at the place of the operation
+
+comment content will not be presrnt inthe java program
+
+*/
